2e36dc6e6a7c43efba0d3cc638f89f8388a1df3e
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-06-02 22:04:50 -0400

    initial commit and added the basic html for the calculator as well as the base functions for adding, subtracting, multiplying, and dividing

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const add = (x, y) => {
+  return x + y;
+}
+
+const subtract = (x, y) => {
+  return x - y;
+}
+
+const multiply = (x, y) => {
+  return x * y;
+}
+
+const divide = (x, y) => {
+  return x / y;
+}
+
+const operate = (operator, x, y) => {
+  
+}
