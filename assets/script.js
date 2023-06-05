2558ac080167c34c7f42018c6486d2cdd2402074
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-06-05 13:21:39 -0400

    basic functionality of calculator completed

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-let numOne = 0;
-let operator = 0;
-let numTwo = 0;
+let numOne = [];
+let operator = [];
+let numTwo = [];
+let displayText = [];
 
 const add = (x, y) => {
-  return x + y;
+  return +x + +y;
 }
 
 const subtract = (x, y) => {
@@ -18,6 +19,74 @@ const divide = (x, y) => {
   return x / y;
 }
 
-const operate = (x, operator, y) => {
-  add(x, y);
+const operate = (x, op, y) => {
+  if (op === "+") {
+    displayText = add(x, y);
+  } else if (op === "-") {
+    subtract(x, y);
+  } else if (op === "*") {
+    multiply(x, y);
+  } else if (op === "/") {
+    divide(x, y);
+  }
 }
+
+const populateDisplay = () => {
+  const DISPLAY = document.querySelector(".display");
+  const NUM_BTNS = document.querySelectorAll(".num-btn");
+  const OP_BTNS = document.querySelectorAll(".op-btn");
+  const EQUALS_BTN = document.querySelector(".equals-btn");
+  const CLEAR_BTN = document.querySelector(".clear-btn");
+
+  for (let i = 0; i < NUM_BTNS.length; i++) {
+    let numBtn = NUM_BTNS[i];
+    numBtn.addEventListener("click", function () {
+      if (operator.length > 0) {
+        numTwo.push(numBtn.textContent);
+        DISPLAY.textContent = numOne.join("") + " " + operator.toString() + " " + numTwo.join("");
+      } else {
+        numOne.push(numBtn.textContent);
+        DISPLAY.textContent = numOne.join("");
+      }
+    });
+  }
+
+  for (let j = 0; j < OP_BTNS.length; j++) {
+    let opBtn = OP_BTNS[j];
+    opBtn.addEventListener("click", function () {
+      if (operator.length === 1) {
+        operator.splice(0, 1);
+        operator.push(opBtn.textContent);
+      } else {
+        operator.push(opBtn.textContent);
+      }
+
+      // resets numTwo if user changes operator
+      if (numTwo.length > 0) {
+        numTwo = [];
+      }
+
+      DISPLAY.textContent = numOne.join("") + " " + operator;
+    });
+  }
+
+  EQUALS_BTN.addEventListener("click", function () {
+    operate(numOne.join(""), operator.toString(), numTwo.join(""));
+    clearCalcStores();
+    DISPLAY.textContent = displayText;
+  });
+
+  CLEAR_BTN.addEventListener("click", function () {
+    clearCalcStores();
+    displayText = [];
+    DISPLAY.textContent = "";
+  });
+}
+
+const clearCalcStores = () => {
+  numOne = [];
+  operator = [];
+  numTwo = [];
+}
+
+populateDisplay();
